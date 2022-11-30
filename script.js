2e453c5789d25c4137f3c4974f5dd73262adf527
Akeem <akeem.mighty@gmail.com>
2022-11-29 20:57:30 -0500

    Created Interest Algorithm + Created Number of Years to Actual Year Algorithm

--- script.js
+++ script.js
@@ -4,5 +4,7 @@ function compute()
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
 }
         
