135b20735ccd9f2640377f9dc24ffef73e91ced9
Akeem <akeem.mighty@gmail.com>
2022-11-30 08:03:25 -0500

    Updated Principal Validation Logic

--- script.js
+++ script.js
@@ -4,16 +4,19 @@ function compute()
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     
-    if(principal < 0)
+    if(principal.value < 0)
     {
         alert("Enter a positive number");
         principal.focus();
         return false;
     }
+    else {
+        var interest = principal.value * years * rate /100;
+        var year = new Date().getFullYear()+parseInt(years);
+        document.getElementById("result").innerHTML="\<br\>"+"If you deposit \<mark\>"+principal.value+"\<mark"+"\\"+"\>,\<br\>at an interest rate of \<mark\>"+rate+"%\<mark"+"\\"+"\>.\<br\>You will receive an amount of \<mark\>"+interest+"\<mark"+"\\"+"\>,\<br\>in the year \<mark\>"+year+"\<mark"+"\\"+"\>\<br\>"
+    }
 
-    var interest = principal.value * years * rate /100;
-    var year = new Date().getFullYear()+parseInt(years);
-    document.getElementById("result").innerHTML="\<br\>"+"If you deposit \<mark\>"+principal.value+"\<mark"+"\\"+"\>,\<br\>at an interest rate of \<mark\>"+rate+"%\<mark"+"\\"+"\>.\<br\>You will receive an amount of \<mark\>"+interest+"\<mark"+"\\"+"\>,\<br\>in the year \<mark\>"+year+"\<mark"+"\\"+"\>\<br\>"
+    
 }
 
 function updateRate()
