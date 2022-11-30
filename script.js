476bfec01d4314df336d680d9f23488caaae5a8d
Akeem <akeem.mighty@gmail.com>
2022-11-30 07:58:11 -0500

    Minor Tweak To Result Highlight In Script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function compute()
 
     var interest = principal.value * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
-    document.getElementById("result").innerHTML="If you deposit \<mark\>"+principal.value+"\<mark\\\>,\<br\>at an interest rate of \<mark\>"+rate+"%\<mark\\\>.\<br\>You will receive an amount of \<mark\>"+interest+"\<mark\\\>,\<br\>in the year \<mark\>"+year+"\<mark\\\>\<br\>"
+    document.getElementById("result").innerHTML="\<br\>"+"If you deposit \<mark\>"+principal.value+"\<mark"+"\\"+"\>,\<br\>at an interest rate of \<mark\>"+rate+"%\<mark"+"\\"+"\>.\<br\>You will receive an amount of \<mark\>"+interest+"\<mark"+"\\"+"\>,\<br\>in the year \<mark\>"+year+"\<mark"+"\\"+"\>\<br\>"
 }
 
 function updateRate()
