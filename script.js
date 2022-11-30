9607ab1a359ad16c3867942f2b28f73d10b8df52
Akeem <akeem.mighty@gmail.com>
2022-11-29 22:12:10 -0500

    Added Principal Validation Functionality

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function compute()
 {
-    var principal = document.getElementById("principal").value;
+    var principal = document.getElementById("principal");
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     
-    var interest = principal * years * rate /100;
+    if(principal < 0)
+    {
+        alert("Enter a positive number");
+        principal.focus();
+        return false;
+    }
+
+    var interest = principal.value * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
-    document.getElementById("result").innerHTML="If you deposit \<mark\>"+principal+"\<mark\\\>,\<br\>at an interest rate of \<mark\>"+rate+"%\<mark\\\>.\<br\>You will receive an amount of \<mark\>"+interest+"\<mark\\\>,\<br\>in the year \<mark\>"+year+"\<mark\\\>\<br\>"
+    document.getElementById("result").innerHTML="If you deposit \<mark\>"+principal.value+"\<mark\\\>,\<br\>at an interest rate of \<mark\>"+rate+"%\<mark\\\>.\<br\>You will receive an amount of \<mark\>"+interest+"\<mark\\\>,\<br\>in the year \<mark\>"+year+"\<mark\\\>\<br\>"
 }
 
 function updateRate()
