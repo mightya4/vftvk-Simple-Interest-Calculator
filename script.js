7e63c491180f842e553e65a20f070905cbc32092
Akeem <akeem.mighty@gmail.com>
2022-11-30 08:05:07 -0500

    Moved Year to Actual Year Conversion Global

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function compute()
     var principal = document.getElementById("principal");
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
-    
+    var year = new Date().getFullYear()+parseInt(years);
     if(principal.value < 0)
     {
         alert("Enter a positive number");
@@ -12,7 +12,6 @@ function compute()
     }
     else {
         var interest = principal.value * years * rate /100;
-        var year = new Date().getFullYear()+parseInt(years);
         document.getElementById("result").innerHTML="\<br\>"+"If you deposit \<mark\>"+principal.value+"\<mark"+"\\"+"\>,\<br\>at an interest rate of \<mark\>"+rate+"%\<mark"+"\\"+"\>.\<br\>You will receive an amount of \<mark\>"+interest+"\<mark"+"\\"+"\>,\<br\>in the year \<mark\>"+year+"\<mark"+"\\"+"\>\<br\>"
     }
 
