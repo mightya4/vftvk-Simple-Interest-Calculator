c1202e14bff53ac89ee6551d4a839afe509b56cb
Akeem <akeem.mighty@gmail.com>
2022-11-29 21:02:23 -0500

    Created Update Rate Functionality in Script.js

--- script.js
+++ script.js
@@ -7,4 +7,10 @@ function compute()
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
 }
+
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
         
